d09ae39b62e6e319b716feb3c293541790ba27ea
AliAbdulloh <74522999+AliAbdullo@users.noreply.github.com>
2023-02-22 07:39:37 +0500

    Fibonacci

---
+++ Lesson04/src/Fibonacci.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int resualt = task(5);
+        System.out.println(resualt);
+        int fib1=task2(3);
+        System.out.println(fib1);
+
+    }
+
+    public static int task(int num) {
+        Scanner scanner = new Scanner(System.in);
+        num = scanner.nextInt();
+        if (num == 0 || num == 1) {
+            return num;
+        }
+        if (num == 2) {
+            return 1;
+        }
+        int fibo_first = 0;
+        int fibo_second = 1;
+        int current = 1;
+        for (int i = 2; i < num; i++) {
+            current = fibo_second + fibo_first;
+            fibo_first = fibo_second;
+            fibo_second = current;
+
+        }
+        return current;
+    }
+    static int task2(int n){
+        if(n <= 1)
+            return n;
+        return task2(n-1) + task2(n-2);
+    }
+}
--- Lesson04/src/SwitchExample2.java
+++ Lesson04/src/SwitchExample2.java
@@ -4,16 +4,16 @@ public class SwitchExample2 {
     public static void main(String[] args) {
         Random random = new Random();
 
-        int monthNumber = random.nextInt(1, 12+1);
+        int monthNumber = random.nextInt(1, 12 + 1);
         System.out.println("monthNumber = " + monthNumber);
 
 
         // switch expression
         String season = switch (monthNumber) {
-            case 12,1,2 -> "winter";
-            case 3,4,5 -> "spring";
-            case 6,7,8 -> "summer";
-            case 9,10,11 -> "autumn (fall)";
+            case 12, 1, 2 -> "winter";
+            case 3, 4, 5 -> "spring";
+            case 6, 7, 8 -> "summer";
+            case 9, 10, 11 -> "autumn (fall)";
             default -> "wrong season number";
         };
 
