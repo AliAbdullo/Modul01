2a36eb931d11c9cc42ff49b7487522abf0416a4d
AliAbdulloh <74522999+AliAbdullo@users.noreply.github.com>
2023-02-28 12:46:24 +0500

    MuntazamIfodalar

---
+++ Lesson09/src/Regex.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+
+
+public class Regex {
+    public static void main(String[] args) {
+        Scanner kiritish = new Scanner(System.in);
+
+
+
+
+
+
+/*
+  int len = str.length();
+  if (len % 2==1){
+    return str.startsWith("xyz", len/2-1) ||
+           str.startsWith("xyz", len/2+2) ||
+           str.startsWith("xyz", len/2);
+  }
+
+  return str.startsWith("xyz", len/2-2) ||
+         str.startsWith("xyz", len/2-1) ||
+         str.startsWith("xyz", len/2);
+
+
+        //1. Kiritilgan satrni uz card karta raqami ekanligini tekshiring
+
+
+        System.out.println("Avtomobil raqamingizni kiriting: ");
+        String num = kiritish.nextLine();
+
+        boolean avtonum= num.matches("(\\d{2}[A-Z][0-9]{3}[A-Z]{2})");
+    }
+*/
+
+
+/*        System.out.println("INN ingizni kiriting: ");
+        String num = kiritish.nextLine();
+
+        boolean number = num.matches("\\d{9}");
+        System.out.println(number);*/
+
+/*        System.out.print("Karta raqamingizni kiriting: ");
+        String Card = kiritish.nextLine();
+
+        boolean humo = Card.matches("8200[0-9]{12}");
+        boolean uzCard= Card.matches("9600[0-9]{12}");
+        if(humo){
+            System.out.println("humo karta");
+        } else if (uzCard) {
+            System.out.println("UzCard");
+        }*/
+
+/*
+//1
+        boolean karta= Card.matches("8600([0-9]{12})");
+        System.out.println(karta);
+*/
+
+/*        System.out.print("Karta raqamingizni kiriting: ");
+        String uzCard = kiritish.nextLine();*/
+
+    }
+}
