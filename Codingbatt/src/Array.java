2083e7f24d7ef0c9f0253c8e55099a45bd452366
AliAbdulloh <74522999+AliAbdullo@users.noreply.github.com>
2023-03-09 21:36:11 +0500

    Exercise
    Signed-off-by: AliAbdulloh <74522999+AliAbdullo@users.noreply.github.com>

--- Codingbatt/src/Array.java
+++ Codingbatt/src/Array.java
@@ -3,8 +3,29 @@ public class Array {
 
         int[] massiv = {4, 9, 5};
         int[] massiv2 = {9, 4, 9, 8, 4};
-        System.out.println();
+//        System.out.println(max(massiv2));
 
+        min(massiv2);
+
+    }
+
+    /*    public static int max(int[] arr) {
+            int max = 0;
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] > max) {
+                    max = arr[i];
+                }
+            }
+            return max;
+        }*/
+    public static void min(int[] arr) {
+        int min = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        System.out.println(min);
     }
 
 }
